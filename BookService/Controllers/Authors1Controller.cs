02e265dabf0b45c77a0b3a3098aedb02048ad916
LiLi <kflili@gmail.com>
2017-08-01 23:41:32 -0400

    add author crud with pure mvc not api

---
+++ BookService/Controllers/Authors1Controller.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookService.Models;
+
+namespace BookService.Controllers
+{
+    public class Authors1Controller : Controller
+    {
+        private BookServiceContext db = new BookServiceContext();
+
+        // GET: Authors1
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Authors.ToListAsync());
+        }
+
+        // GET: Authors1/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Author author = await db.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            return View(author);
+        }
+
+        // GET: Authors1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authors1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name")] Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Authors.Add(author);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(author);
+        }
+
+        // GET: Authors1/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Author author = await db.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Authors1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name")] Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(author).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(author);
+        }
+
+        // GET: Authors1/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Author author = await db.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return HttpNotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Authors1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Author author = await db.Authors.FindAsync(id);
+            db.Authors.Remove(author);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
